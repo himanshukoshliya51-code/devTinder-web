ce0ea2e2c295167fb12be9f752629834591aff58
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-02-18 22:50:08 +0530

    Build the user card on feed

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const UserCard = ({user}) => {
+   // console.log(user);
+   const{firstName, lastName, photoUrl, age, gender,  about }=user;
+  return (
+   <div className="card bg-base-300 w-96 shadow-sm">
+  <figure>
+    <img
+      src={user.photoUrl}
+      alt="Photo" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{firstName + " " + lastName}</h2>
+    {age && gender&& <p>{age + ", "+ gender}r</p>}
+    <p>{about}</p>
+    <div className="card-actions justify-center ">
+              <button className="btn btn-primary mx-4">Ignore</button>
+      <button className="btn btn-secondary mx-4">Interested</button>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default UserCard
