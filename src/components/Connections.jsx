363d44c3a5fdf41e31f46da80544cd26d1ff22f6
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-02-19 08:21:29 +0530

    Created connection page

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,19 +1,52 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect } from "react";
 import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addConnections } from "../utils/connectionSlice";
 
 const Connections = () => {
+  const connections = useSelector((store) => store.connections);
+  const dispatch = useDispatch();
   const fetchConnections = async () => {
     try {
-      const res = await axios.get(BASE_URL + "/connections", {
+      const res = await axios.get(BASE_URL + "/user/connections", {
         withCredentials: true,
       });
-      console.log(res);
+      console.log(res.data);
+      dispatch(addConnections(res.data.data));
     } catch (err) {
       console.log(err);
     }
   };
-  return <div></div>;
+
+  useEffect(() => {
+    fetchConnections();
+  }, []);
+  if (!connections) return;
+  if (connections.length === 0)
+    return <h1 className="text-bold text-2xl">No connections found</h1>;
+  return (
+    <div className="text-center my-10">
+      <h1 className="text-bold text-white text-4xl">Connections</h1>
+      {connections.map((connection) => {
+        const { firstName, lastName, photoUrl, age, gender, about } =
+          connection;
+        return (
+          <div className="flex m-4 p-4 border rounded-lg bg-base-300 w-1/2 mx-auto">
+            <div>
+              <img alt="photo" className="w-20 h-20 rounded-full " src={photoUrl} />
+            </div>
+            <div className="text-left mx-5">
+              <h2 className="font-bold text-xl">{firstName + " " + lastName}</h2>
+              {age && gender && <p>{age + ", " + gender}</p>}
+              <p>{about}</p>
+              
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Connections;
