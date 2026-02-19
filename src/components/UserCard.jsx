4bb4733876d27fa90ac23265c306ae0615ade98f
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-02-19 20:06:21 +0530

    accept/ignore the user card from the feed

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,26 +1,41 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
 
-const UserCard = ({user}) => {
-   // console.log(user);
-   const{firstName, lastName, photoUrl, age, gender,  about }=user;
+const UserCard = ({ user }) => {
+  // console.log(user);
+  const { _id,firstName, lastName, photoUrl, age, gender, about } = user;
+  const dispatch = useDispatch();
+  const handleSendRequest = async (status, userId) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/send/" + status + "/" + userId,
+        {},
+        { withCredentials: true },
+      );
+      dispatch(removeUserFromFeed(userId));
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-   <div className="card bg-base-300 w-96 shadow-sm">
-  <figure>
-    <img
-      src={photoUrl}
-      alt="Photo" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">{firstName + " " + lastName}</h2>
-    {age && gender&& <p>{age + ", "+ gender}</p>}
-    <p>{about}</p>
-    <div className="card-actions justify-center ">
-              <button className="btn btn-primary mx-4">Ignore</button>
-      <button className="btn btn-secondary mx-4">Interested</button>
+    <div className="card bg-base-300 w-96 shadow-sm">
+      <figure>
+        <img src={photoUrl} alt="Photo" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        {age && gender && <p>{age + ", " + gender}</p>}
+        <p>{about}</p>
+        <div className="card-actions justify-center ">
+          <button className="btn btn-primary mx-4"onClick={()=>handleSendRequest("ignored",_id)}>Ignore</button>
+          <button className="btn btn-secondary mx-4"onClick={()=>handleSendRequest("interested",_id)}>Interested</button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default UserCard
+export default UserCard;
