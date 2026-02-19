7ba0f47f7d1fec032d1f322d06a64a9237a283d9
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-02-19 09:13:32 +0530

    accept/reject connection request

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -29,20 +29,26 @@ const Connections = () => {
     <div className="text-center my-10">
       <h1 className="text-bold text-white text-4xl">Connections</h1>
       {connections.map((connection) => {
-        const { _id,firstName, lastName, photoUrl, age, gender, about } =
+        const { _id, firstName, lastName, photoUrl, age, gender, about } =
           connection;
         return (
-          <div 
-           key={_id}
-          className="flex m-4 p-4 border rounded-lg bg-base-300 w-1/2 mx-auto">
+          <div
+            key={_id}
+            className="flex m-4 p-4 border rounded-lg bg-base-300 w-1/2 mx-auto"
+          >
             <div>
-              <img alt="photo" className="w-20 h-20 rounded-full " src={photoUrl} />
+              <img
+                alt="photo"
+                className="w-20 h-20 rounded-full "
+                src={photoUrl}
+              />
             </div>
             <div className="text-left mx-5">
-              <h2 className="font-bold text-xl">{firstName + " " + lastName}</h2>
+              <h2 className="font-bold text-xl">
+                {firstName + " " + lastName}
+              </h2>
               {age && gender && <p>{age + ", " + gender}</p>}
               <p>{about}</p>
-              
             </div>
           </div>
         );
