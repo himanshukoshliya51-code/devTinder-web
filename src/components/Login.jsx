551a10280c66ec7b7b8df665632233aab9079597
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-02-19 21:03:09 +0530

    Added Sign up form and some bug fixes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,16 +6,20 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("himanshu@gmail.com");
-  const [password, setPassword] = useState("Himanshu@143");
-  const [error,setError] = useState("");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const [isLoginForm, setIsLoginForm] = useState(true);
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        BASE_URL+"/login",
+        BASE_URL + "/login",
         {
           emailId,
           password,
@@ -26,17 +30,53 @@ const Login = () => {
       return navigate("/");
     } catch (err) {
       setError(err?.response?.data || "Something went wrong");
-  
     }
-  };
+    };
+
+    const handleSignUp = async () => {
+      try {
+        const res =await axios.post(
+          BASE_URL + "/signup",
+          { firstName, lastName, emailId, password },
+          { withCredentials: true },
+        );
+         dispatch(addUser(res.data.data));
+      return navigate("/profile");
+      } catch (err) {
+        setError(err?.response?.data || "Something went wrong");
+      }
+    };
   return (
     <div className="flex justify-center my-20">
       <div className="card w-96 bg-base-300 card-lg shadow-sm">
         <div className="card-body">
-          <h2 className="card-title">LOGIN </h2>
+          <h2 className="card-title">{isLoginForm ? "LOGIN" : "SIGNUP"}</h2>
           <div className="my-3">
+            {!isLoginForm && (
+              <>
+                {" "}
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">First Name</legend>
+                  <input
+                    type="text"
+                    value={firstName}
+                    className="input"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">Last Name</legend>
+                  <input
+                    type="text"
+                    value={lastName}
+                    className="input"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </fieldset>
+              </>
+            )}
             <fieldset className="fieldset">
-              <legend className="fieldset-legend">Email ID</legend>
+              <legend className="fieldset-legend">Email Id</legend>
               <input
                 type="text"
                 value={emailId}
@@ -48,7 +88,7 @@ const Login = () => {
             <fieldset className="fieldset">
               <legend className="fieldset-legend">Password</legend>
               <input
-                type="text"
+                type="password"
                 value={password}
                 className="input"
                 onChange={(e) => setPassword(e.target.value)}
@@ -57,10 +97,21 @@ const Login = () => {
           </div>
           <p className="text-red-500">{error}</p>
           <div className="justify-center card-actions">
-            <button className="btn btn-primary" onClick={handleLogin}>
-              Login
+            <button
+              className="btn btn-primary"
+              onClick={isLoginForm ? handleLogin : handleSignUp}
+            >
+              {isLoginForm ? "Login" : "Sign Up"}
             </button>
           </div>
+          <p
+            className="m-auto cursor-pointer py-2"
+            onClick={() => setIsLoginForm((value) => !value)}
+          >
+            {isLoginForm
+              ? "New User ? Signup Here"
+              : "Existing User? Login Here"}
+          </p>
         </div>
       </div>
     </div>
